da5921dade8efe135493c4e97239f2c2496f8056
Florian Durand <floriandurand09@gmail.com>
2020-12-20 17:54:47 +0100

    Add some java docs

--- local-kube/src/main/java/fr/umlv/localkube/component/DeadContainerInterceptor.java
+++ local-kube/src/main/java/fr/umlv/localkube/component/DeadContainerInterceptor.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Interceptor class to process before each HTTP request.
+ */
 @Component
 public class DeadContainerInterceptor extends HandlerInterceptorAdapter {
 
@@ -18,6 +21,15 @@ public class DeadContainerInterceptor extends HandlerInterceptorAdapter {
         this.applicationService = applicationService;
     }
 
+    /**
+     * Removes all dead applications from applicationService before each HTTP request.
+     *
+     * @param request current HTTP request
+     * @param response current HTTP response
+     * @param handler chosen handler to execute, for type and/or instance evaluation
+     * @return true if the execution chain should proceed with the next interceptor or the handler itself. Else, DispatcherServlet assumes that this interceptor has already dealt with the response itself.
+     * @throws Exception in case of errors
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         applicationService.removeAllDeadDockerInstance();
--- local-kube/src/main/java/fr/umlv/localkube/configuration/DataBaseProperties.java
+++ local-kube/src/main/java/fr/umlv/localkube/configuration/DataBaseProperties.java
@@ -3,33 +3,70 @@ package fr.umlv.localkube.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Data class for the database properties, based on the "application.properties" file.
+ */
 @Configuration
 @ConfigurationProperties(prefix = "database")
 public class DataBaseProperties {
+
+    /**
+     * Database url property.
+     */
     private String url;
+    /**
+     * Database username property.
+     */
     private String username;
+    /**
+     * Database password property.
+     */
     private String password;
 
+    /**
+     * Returns the database password.
+     * @return Database password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the database username.
+     * @return Database username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the database url.
+     * @return Database url.
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the password property.
+     * @param password Database password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Sets the url property.
+     * @param url Database url.
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Sets the username property.
+     * @param username Database username.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
