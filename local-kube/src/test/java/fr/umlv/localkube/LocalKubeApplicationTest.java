dc6841c51339e8d6dd410eeef49ab7f6d727bd4a
Florian Durand <floriandurand09@gmail.com>
2020-12-30 14:21:43 +0100

    Debug test for jar creation

--- local-kube/src/test/java/fr/umlv/localkube/LocalKubeApplicationTest.java
+++ local-kube/src/test/java/fr/umlv/localkube/LocalKubeApplicationTest.java
@@ -2,12 +2,16 @@ package fr.umlv.localkube;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class LocalKubeApplicationTest {
 
     @Test
     void contextLoads(){
     }
+
+
 
 }
